d43758cb3ec63fba37d93ede2c7dca8c8cd1741b
falkTX <falktx@falktx.com>
2025-06-17 10:20:20 +0200

    Disable audio component for usb1, not enough bandwidth
    Signed-off-by: falkTX <falktx@falktx.com>

--- drivers/usb/gadget/function/f_uac2.c
+++ drivers/usb/gadget/function/f_uac2.c
@@ -1164,7 +1164,7 @@ afunc_bind(struct usb_configuration *cfg, struct usb_function *fn)
 
 	setup_descriptor(uac2_opts);
 
-	ret = usb_assign_descriptors(fn, fs_audio_desc, hs_audio_desc, ss_audio_desc,
+	ret = usb_assign_descriptors(fn, NULL, hs_audio_desc, ss_audio_desc,
 				     ss_audio_desc);
 	if (ret)
 		goto err_free_fu;
